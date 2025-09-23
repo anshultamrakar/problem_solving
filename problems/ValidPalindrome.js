8a71314941da523f956944afc6c2fd4ca0f7ff79
Anshul <anshul@belgiumwebnet.com>
2025-09-23 15:30:21 +0530

    valid palindrome

--- problems/ValidPalindrome.js
+++ problems/ValidPalindrome.js
@@ -1,5 +1,18 @@
 // https://leetcode.com/problems/valid-palindrome/description/
+const string = "A man, a plan, a canal: Panama"
 
-function isPalindrome(s){
-
+// Approach1
+function validPalindrome(s){
+  s = s.toLowerCase()
+  let filteredString = ""
+  let rev = ""
+  for(let i = 0 ; i < s.length ; i++){
+    if(s[i].match(/[a-z0-9]/i)){
+      filteredString+= s[i]
+      rev = s[i] + rev
+    }
+  }
+  return filteredString === rev
 }
+
+console.log(validPalindrome(string))
