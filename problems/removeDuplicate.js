516d134d9c917b7a7916478b6f00f8fa55ec6c9e
Anshul <anshul@belgiumwebnet.com>
2025-10-07 13:12:06 +0530

    remove duplicate approch 2

--- problems/removeDuplicate.js
+++ problems/removeDuplicate.js
@@ -1,15 +1,35 @@
 const array = [1,2,2,2,3,4,4,5]
 
+// Approach 1
+// function removeDuplicate(arr){
+//    let result = [] 
+//     for(let i = 0 ; i < arr.length ; i++){
+//       if(!result.includes(arr[i])){
+//         result.push(arr[i])
+//       }
+//     }
 
-function removeDuplicate(arr){
-   let result = [] 
-    for(let i = 0 ; i < arr.length ; i++){
-      if(!result.includes(arr[i])){
-        result.push(arr[i])
+//     return result
+// }
+
+
+
+// Approach 2
+function removeDuplicate(arr) {
+  let uniqueArr = []
+  for (let i = 0; i < arr.length; i++) {
+      let isDuplicate = false
+    for (j = 0; j < uniqueArr.length; j++) {
+      if (arr[i] === uniqueArr[j]) {
+        isDuplicate = true
+        break
       }
     }
-
-    return result
+    if (!isDuplicate) {
+      uniqueArr.push(arr[i])
+    }
+  }
+  return uniqueArr
 }
 
 console.log(removeDuplicate(array))
